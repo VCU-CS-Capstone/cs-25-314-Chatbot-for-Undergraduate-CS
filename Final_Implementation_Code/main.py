2d6f5b742ea9928f6a6f02a28d7ccdef40e7cd20
Israel Alan Agoe-Sowah <agoesowahia@vcu.edu>
2025-03-26 12:25:31 -0400

    Implemented  changes and now runs smoothly

--- Final_Implementation_Code/main.py
+++ Final_Implementation_Code/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from chat2 import Chatbot
+from chat3 import Chatbot
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -25,4 +25,4 @@ async def chat(request: ChatRequest):
     return {"reply": bot_reply}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
