f8062ec8890d84aae0dd155c45b645b3ec253f88
Eric Simoni <simonie2@vcu.edu>
2025-04-02 14:45:33 -0400

    deleted my staging folder

--- Staging/Eric Staging/UX/test.py
+++
@@ -1,9 +0,0 @@
-def chatbot_response(user_input):
-    if "hello" in user_input.lower():
-        return "Hello! How can I help you today?"
-    elif "help" in user_input.lower():
-        return "Sure! What do you need help with?"
-    elif "easter egg" in user_input.lower():
-        return "I'm Batman!"
-    else:
-        return "I'm not sure how to respond to that."
